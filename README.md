8c758d24ccd2bcf9bcde487ee2c912a1358baa93
chillcrunch37 <chillcrunch37@gmail.com>
2021-06-20 18:58:46 -0400

    Solved Push???3

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 #HelloWorld
 #this is an added line
 #this is another added line
+Improved comment from VSCode 
+
+Test VSCode pushes
+ 6/19/2021 2:52:38 PM
+New modification  6/20/2021 11:35:53 AM
+
--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
 #this is the shell-bang line
-print('Hello World')
+print('Hello World from main')
 
 def hi():
-    print('Hello World')
+    print('Hello, World! - from definition')
+
+class HelloLanguages():
+    def __init__(self, *args):
+        super(HelloLanguages, self).__init__(*args)
+    def printHello(self, language):
+        print('Hello, {}! - from class method'.format(language))
+
+        print(f'Hello, {language}! - from class method')
+        
+        print('Hello, %s! - from class method' % (language))
+
 if __name__ == '__main__':
     hi()
